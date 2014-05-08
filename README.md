d69bb33f65f5188a59d31c8617bf52d6507aa39a
David Cantrell <david.l.cantrell@gmail.com>
2014-05-08 13:18:53 -0400

    Initial import.

--- README.md
+++ README.md
@@ -2,3 +2,47 @@ socialfixer
 ===========
 
 Social Fixer configuration
+Licensed under the Apache 2.0 License (is that even necessary?)
+David Cantrell <david.l.cantrell@gmail.com>
+
+
+WHAT IS SOCIAL FIXER?
+=====================
+
+Social Fixer makes FaceBook tolerable in Firefox.  Maybe other browsers, but I
+don't know.  I use Firefox.
+
+    http://socialfixer.com/
+
+
+HOW DO I USE THIS CONFIGURATION?
+================================
+
+1) Install Social Fixer per the instructions on their site.
+2) Open socialfixer.js in an editor, select all text, copy to clipboard.
+3) Go to www.facebook.com and open the Social Fixer configuration.  It is
+   called Edit Social Fixer Options.
+4) Click the User Preferences tab.
+5) Paste the entire contents from socialfixer.js in to the one field on
+   this tab.  Overwrite anything already there.
+6) Click Save.
+
+You will lose any existing customizations you've done to Social Fixer, but there's no easy way to merge socialfixer.js with an existing configuration.  If I'm wrong and you know how, let me know.
+
+
+HOW DO I UPDATE socialfixer.js?
+===============================
+
+1) Go to www.facebook.com and open the Social Fixer configuration.  It is
+   called Edit Social Fixer Options.
+2) Click the User Preferences tab.
+3) Select all text in the one field on this tab and copy it to the clipboard.
+4) Open socialfixer.js in a text editor, delete all text.
+5) Paste the clipboard contents.
+6) Save.
+
+
+CONTRIBUTE BACK
+===============
+
+Fork this repo and enhance my configuration.  Send me merge requests.
---
+++ socialfixer.js
@@ -0,0 +1,179 @@
+{
+ "installed_on_5": 1389368354844,
+ "last_message_check": 1389888368584,
+ "tip_show_after": 1399645063203,
+ "blog_post_latest": 547,
+ "version": 9,
+ "hide_options_indicator": true,
+ "pagelet_toggle": true,
+ "filters": [
+  {
+   "criteria": {
+    "selector": "a[href*=\"zimbio.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"buzzfeed.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"theblaze.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"quickmeme.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"viralnova.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"rightthisminute.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"9gag.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"upworthy.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"huff.to\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"huffpost.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"huffingtonpost.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"huffingtonpost.co.uk\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "selector": "a[href*=\"slightlyviral.com\"]"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "regex": "/(huff).to/"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "regex": "/(huffingtonpost).co.uk/"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "regex": "/(slightlyviral|theblaze|huffingtonpost|huffpost|buzzfeed|9gag|viralnova|rightthisminute|upworthy|quickmeme).com/"
+   },
+   "actions": {
+    "move_to_tab": "junk"
+   },
+   "disabled": false
+  },
+  {
+   "criteria": {
+    "actrs": {
+     "639548757": "Alison Buck",
+     "1068640102": "Amber Imel Graner",
+     "741489660": "Ruth Suehle",
+     "100000611805024": "Scott Stallings"
+    }
+   },
+   "actions": {
+    "move_to_tab": "noisy"
+   },
+   "disabled": false
+  }
+ ],
+ "cp_show_more": true,
+ "filter_groups": true,
+ "always_show_tabs": true,
+ "chat_compact": true,
+ "show_image_previews": false,
+ "auto_subscribe_news": false,
+ "timeline_show_panel": false,
+ "update_show_after": 1399594159949,
+ "comment_reply_first_name_only": true,
+ "fix_comments": true
+}
